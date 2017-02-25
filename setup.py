3a565da2318a16c9911ffd38aa3c9514d4f79773
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-25 17:38:55 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="geometrica",
+ version="0.1.0",
+ description="A pure-Python geometry library",
+ url="https://geometrica.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Mathematics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+ ],
+ keywords="geometry trigonometry coordinates shapes",
+ packages=["geometrica"]
+)
