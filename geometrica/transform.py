e821b7f39be264440f381ce8f8462db3ba17689d
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-25 17:58:17 +0000

    Add transformation module from numerus

---
+++ geometrica/transform.py
@@ -0,0 +1,70 @@
+"""Contains functions for manipulating Cartesian coordinates."""
+
+from math import radians, sin, cos
+from matrices.checks import are_numeric, is_numeric
+from matrices import create_vertex, Matrix
+
+def translate(points, x, y, z):
+    """Takes a set of coordinates and translates them in three dimensional
+    space.
+
+    The points must be a list (or tuple, or any collection really) of
+    coordinates in the form ``(x, y, z)``.
+
+    An example would be ``translate([(1, 1, 1), (2, 2, 2)], 5, 5, 5)``.
+
+    :param points: A collection of (x, y, z) coordinates.
+    :param Number x: The distance to move the points in the x direction.
+    :param Number y: The distance to move the points in the y direction.
+    :param Number z: The distance to move the points in the z direction.
+    :returns: The translated coordinates."""
+
+    if not are_numeric(x, y, z):
+        raise TypeError("Translation parameters must be numeric, not '%s'" % (
+         str((x, y, z))
+        ))
+    return tuple([tuple([px + x, py + y, pz + z]) for px, py, pz in points])
+
+
+def rotate(points, axis, angle):
+    """Takes a set of coordinates and rotates them around one of the axes by a
+    specified angle. The rotation performed is right-handed.
+
+    The points must be a list (or tuple, or any collection really) of
+    coordinates in the form ``(x, y, z)``.
+
+    An example would be ``rotate([(1, 1, 1), (2, 2, 2)], "x", 45)``.
+
+    :param points: A collection of (x, y, z) coordinates.
+    :param str axis: The axis to rotate around. Accepted values are `"x"`,\
+    `"y"` or `"z"`.
+    :param Number angle: The angle in degrees to rotate by.
+    :returns: The translated coordinates."""
+
+    if not is_numeric:
+        raise TypeError("angle must be numeric, not '%s'" % str(angle))
+    angle = radians(angle)
+    points = [create_vertex(*point) for point in points]
+    matrix = None
+    if axis == "x":
+        matrix = Matrix(
+         (1, 0, 0),
+         (0, cos(angle), -sin(angle)),
+         (0, sin(angle),  cos(angle))
+        )
+    elif axis == "y":
+        matrix = Matrix(
+         (cos(angle), 0, sin(angle)),
+         (0, 1, 0),
+         (-sin(angle), 0, cos(angle))
+        )
+    elif axis == "z":
+        matrix = Matrix(
+         (cos(angle), -sin(angle), 0),
+         (sin(angle), cos(angle), 0),
+         (0, 0, 1)
+        )
+    else:
+        raise ValueError("axis can only be 'x', 'y' or 'z', not %s" % axis)
+    new_points = [matrix * point for point in points]
+    return tuple([point.columns()[0] for point in new_points])
---
+++ tests/test_transformations.py
@@ -0,0 +1,91 @@
+from unittest import TestCase
+from geometrica.transform import translate, rotate
+
+class TransformationTest(TestCase):
+
+    def setUp(self):
+        self.points = [(1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1)]
+
+
+    def assertPointsAlmostEqual(self, points1, points2):
+        self.assertEqual(len(points1), len(points2))
+        for index, point1 in enumerate(points1):
+            point2 = points2[index]
+            self.assertEqual(len(point1), len(point2), 3)
+            self.assertAlmostEqual(point1[0], point2[0], delta=0.0005)
+            self.assertAlmostEqual(point1[1], point2[1], delta=0.0005)
+            self.assertAlmostEqual(point1[2], point2[2], delta=0.0005)
+
+
+
+class TranslationTests(TransformationTest):
+
+    def test_can_translate(self):
+        self.assertEqual(
+         translate(self.points, 3, -2, 8),
+         ((4, -1, 9), (5, -1, 9), (6, -1, 9), (7, -1, 9), (8, -1, 9))
+        )
+
+
+    def translation_directions_must_be_numeric(self):
+        with self.assertRaises(TypeError):
+            translate(self.points, "3", -2, 8)
+        with self.assertRaises(TypeError):
+            translate(self.points, 3, "-2", 8)
+        with self.assertRaises(TypeError):
+            translate(self.points, 3, -2, "8")
+
+
+
+class RotationTests(TransformationTest):
+
+    def test_can_rotate_around_x_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "x", 180),
+         ((1, -1, -1), (2, -1, -1), (3, -1, -1), (4, -1, -1), (5, -1, -1))
+        )
+
+
+    def test_can_rotate_around_x_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "x", 90),
+         ((1, -1, 1), (2, -1, 1), (3, -1, 1), (4, -1, 1), (5, -1, 1))
+        )
+
+
+    def test_can_rotate_around_y_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "y", 180),
+         ((-1, 1, -1), (-2, 1, -1), (-3, 1, -1), (-4, 1, -1), (-5, 1, -1))
+        )
+
+
+    def test_can_rotate_around_y_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "y", 90),
+         ((1, 1, -1), (1, 1, -2), (1, 1, -3), (1, 1, -4), (1, 1, -5))
+        )
+
+
+    def test_can_rotate_around_z_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "z", 180),
+         ((-1, -1, 1), (-2, -1, 1), (-3, -1, 1), (-4, -1, 1), (-5, -1, 1))
+        )
+
+
+    def test_can_rotate_around_z_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "z", 90),
+         ((-1, 1, 1), (-1, 2, 1), (-1, 3, 1), (-1, 4, 1), (-1, 5, 1))
+        )
+
+
+    def test_axis_must_be_x_or_y_or_z(self):
+        with self.assertRaises(ValueError):
+            rotate(self.points, "a", 90)
+
+
+    def test_angle_must_be_numeric(self):
+        with self.assertRaises(TypeError):
+            rotate(self.points, "x", "90")
